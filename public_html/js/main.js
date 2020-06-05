09460db672d0f8546382649bc1e4a406a89ded9f
Alessandro Bernardi <alexbernardi360@gmail.com>
2020-06-05 02:30:02 +0200

    Messages are displayed in the chat.

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,1 +1,38 @@
 const socket = io();
+
+const msg_form = document.getElementById('msg_form');
+
+// Run when the server emits a message
+socket.on('message', message => {
+    console.log(message);
+
+    // Add the new message on the screen
+    printMessage(message);
+});
+
+// Event on submitting  message
+msg_form.addEventListener('submit', event => {
+    // Prevent the default behavior
+    event.preventDefault();
+
+    const message = event.target.elements.message.value;
+    msg_form.reset();   // Clear the form from the submitted message;
+
+    console.log(`Message sent: ${message}`);
+
+    // Send the message to the server
+    socket.emit('newMessage', message);
+});
+
+// Create a div with the new message to append in the chat
+function printMessage(message) {
+    let newMsg = document.createElement('div');
+    let attribute = document.createAttribute('class');
+    attribute.value = 'container-fluid msg';
+    newMsg.setAttributeNode(attribute);
+    newMsg.innerHTML = `    <p class="font-weight-bold">Frank</p>
+                            <p>${message}</p>
+                            <span class="time-right">11:00</span>`;
+    let chat = document.getElementById('chat');
+    chat.appendChild(newMsg);
+}
--- server.js
+++ server.js
@@ -7,17 +7,36 @@ const app       = express();
 const server    = http.createServer(app);
 const io        = socketio(server);
 
-// Define public_html
-app.use(express.static(path.join(__dirname, 'public_html')));
-
-// Run when client connects
-io.on('connection', socket => {
-    console.log('New WS connection...');
-});
-
-
 const PORT = 8080 || process.env.PORT;
-
 const listener = server.listen(PORT, () =>
     console.log(`The server is listening on port: ${listener.address().port}`)
 );
+
+// Define public_html
+app.use(express.static(path.join(__dirname, 'public_html')));
+
+// Run when a client connects
+io.on('connection', socket => {
+    console.log('New WS connection...');
+
+    // Send to the client who connected
+    socket.emit('message', 'Welcome to net-rooms');
+
+    // Broadcast when a user connects
+    // Send to everyone except those who connected
+    socket.broadcast.emit('message', 'New user connected.');
+
+    // Run when a client disconnets
+    socket.on('disconnect', () => {
+        // Send to all connected clients
+        io.emit('message', 'A user has disconnected.');
+        console.log('A user has disconnected.');
+    });
+
+    // Run when a client emit newMessage
+    socket.on('newMessage', message => {
+        console.log(message);
+        // Sending the received message to all connected clients
+        io.emit('message', message);
+    });
+});
