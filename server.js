cc7deee61c861378be09674f53dbb9a23a402253
Alessandro Bernardi <alexbernardi360@gmail.com>
2020-06-05 02:33:43 +0200

    Small bugfix on the server port.

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app       = express();
 const server    = http.createServer(app);
 const io        = socketio(server);
 
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 const listener = server.listen(PORT, () =>
     console.log(`The server is listening on port: ${listener.address().port}`)
 );
